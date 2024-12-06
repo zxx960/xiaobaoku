37c92d31f1e2f4be670ed6ab0eb44690cae49aeb
zxx <zxx960@qq.com>
2024-12-07 00:52:22 +0800

    修改图标

--- src/data/categories.js
+++ src/data/categories.js
@@ -3,7 +3,7 @@ export const categories = [
     title: '所有工具',
     items: [
       { name: '天涯神贴播客', url: 'https://podcast.tianyashentie.org/', icon: '📻' },
-      { name: '周杰伦全集', url: 'https://zhoujielun.xiaobaoku.cc/', icon: '📻' },
+      { name: '周杰伦全集', url: 'https://zhoujielun.xiaobaoku.cc/', icon: '🎵' },
       { name: '郭德纲相声', url: 'https://guodegang.xiaobaoku.cc/', icon: '📻' },
       { name: '黑神话悟空影神图', url: 'https://yingshentu.xiaobaoku.cc/', icon: '🖼️' },
     ]
