ac458ec5d1a443373da23025574bcc877a3e5e3f
zxx <zxx960@qq.com>
2024-12-07 02:34:43 +0800

    添加

--- src/data/categories.js
+++ src/data/categories.js
@@ -6,6 +6,7 @@ export const categories = [
       { name: '周杰伦全集', url: 'https://zhoujielun.xiaobaoku.cc/', icon: '🎵' },
       { name: '郭德纲相声', url: 'https://guodegang.xiaobaoku.cc/', icon: '📻' },
       { name: '黑神话悟空影神图', url: 'https://yingshentu.xiaobaoku.cc/', icon: '🖼️' },
+      { name: '九宫格图片分割器', url: 'https://grid-image.xiaobaoku.cc/', icon: '🖼️' },
     ]
   }
 ];
